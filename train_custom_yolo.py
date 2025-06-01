4becbb6321cb87f995fcecfbc19a5b9ceb08689e
seda savas <svs.seda60@gmail.com>
2025-06-01 21:04:05 +0300

    create train_custom_yolo.py

---
+++ train_custom_yolo.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, './ultralytics')
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # Model ve veri yolu
+    model = YOLO('D:/Projects/object_detect_v8/ultralytics/ultralytics/cfg/models/v8/yolov8-cbam.yaml')  # veya başka bir yolov8 ağırlığı
+
+    # Eğitim parametreleri D:\Projects\object_detect_v8\ultralytics\ultralytics\cfg\models\v8\yolov8-ghost-p2.yaml
+    model.train(
+        data='dataset/data.yaml',  # Eğitim verisi için YAML dosyası
+        epochs=15,           # Eğitim epoch sayısı
+        imgsz=640,           # Görüntü boyutu
+        batch=4,            # Batch size
+    )
